225941823db42f1caae7a873de767775ce037fe5
root <dingjingzhen@163.com>
2021-07-27 06:40:52 +0000

    Solve the problem of initialization error when model_dict is confused

--- python/eet/fairseq/transformer.py
+++ python/eet/fairseq/transformer.py
@@ -386,7 +386,9 @@ class EETTransformerDecoder():
             model_dict[k] = v
         from itertools import groupby
         # Intercept k,num = length of 'decoder.layers.**'=17; If your weight name has changed please change it here
-        layer_model_dict = {k: dict(v) for k, v in groupby(list(model_dict.items()), lambda item: item[0][:FROM_TORCH_PARAM_LEN])}
+        model_dict_list = list(model_dict.items())
+        model_dict_list.sort(key = lambda item: item[0][:FROM_TORCH_PARAM_LEN])
+        layer_model_dict = {k: dict(v) for k, v in groupby(model_dict_list, lambda item: item[0][:FROM_TORCH_PARAM_LEN])}
 
         device = "cuda:0"
         activation_fn = args.activation_fn
@@ -453,7 +455,9 @@ class EETTransformerDecoder():
             model_dict[k] = v
         from itertools import groupby
         # Intercept k,num = length of 'decoder.layers.**'=17; If your weight name has changed please change it here
-        layer_model_dict = {k: dict(v) for k, v in groupby(list(model_dict.items()), lambda item: item[0][:FROM_BUFFER_PARAM_LEN])}
+        model_dict_list = list(model_dict.items())
+        model_dict_list.sort(key = lambda item: item[0][:FROM_TORCH_PARAM_LEN])
+        layer_model_dict = {k: dict(v) for k, v in groupby(model_dict_list, lambda item: item[0][:FROM_BUFFER_PARAM_LEN])}
 
         device = "cuda:0"
         activation_fn = args.activation_fn
